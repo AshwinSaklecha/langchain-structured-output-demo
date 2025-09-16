da6f962a9807ce927db5541e8ffc7681f0c835e5
Ashwin Saklecha <ashwinsaklecha2004@gmail.com>
2025-09-17 00:05:38 +0530

    strcutured output using TypedDict

--- with_structured_output_typeddict.py
+++ with_structured_output_typeddict.py
@@ -1,14 +1,19 @@
+from token import OP
 from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
-from typing import TypedDict, Annotated
+from typing import TypedDict, Annotated, Literal, Optional
 
 load_dotenv()
 
 model = ChatGoogleGenerativeAI(model = "gemini-2.0-flash")
 
 class Review(TypedDict):
+    key_themes: Annotated[list[str], "find out key themes discussed in the review"]
     summary: Annotated[str, "A brief summary of what is said in review"]
-    sentiment: Annotated[str, "What is the sentiment of the review - positive, negative or neutral"]
+    sentiment: Annotated[Literal["positive", "negative"], "What is the sentiment of the review - positive or negative"]
+    pros: Annotated[Optional[list[str]], "write down all the pros of this product"]
+    cons: Annotated[Optional[list[str]], "write down all the cons of this product"]
+
 
 structured_model = model.with_structured_output(Review)
 
